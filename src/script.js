2160d6130e0069e32e4bb866ba479cef88023f98
rmdigintel <rm@digintel.ru>
2023-01-17 16:22:59 +0300

    Add alert

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ tg.expand(); //расширяем на все окно
 
 btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
   // tg.sendData("Данные заполнены"); 
-  // alert(`Данные для пользователя ${tg.initDataUnsafe.user.username} заполнены`);
+  alert(JSON.stringify(tg.initDataUnsafe.user.username));
   tg.sendData("Данные заполнены"); 
   tg.close();
 });
