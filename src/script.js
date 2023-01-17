78e3436def9477d8482c0a87c404e31e250a3302
rmdigintel <rm@digintel.ru>
2023-01-17 16:25:33 +0300

    Correct alert

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ tg.expand(); //расширяем на все окно
 
 btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
   // tg.sendData("Данные заполнены"); 
-  alert(JSON.stringify(tg.initDataUnsafe.user.username));
+  alert(JSON.stringify(tg.initDataUnsafe));
   tg.sendData("Данные заполнены"); 
   tg.close();
 });
